cf5b11ccae39eb64176397b05e6fb29a17e9d521
abdellah <Abdellah.chehri14@gmail.com>
2024-12-08 10:51:09 +0100

    feat: push chnage to main

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,7 +4,6 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   clerkUserId: text("username").notNull().unique(),
-
   createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
   updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`),
 });
