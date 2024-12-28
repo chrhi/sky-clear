2ce6f7cc4896bf819bb4fa9720d198ddde08c993
abdellah <Abdellah.chehri14@gmail.com>
2024-12-28 10:53:01 +0100

    feat(backend): create connect server actions

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,19 +1,8 @@
-import { getConnectedAccount, connectBlueSkyAccount } from "@/actions/connect";
+// import { getConnectedAccount, connectBlueSkyAccount } from "@/actions/connect";
 import PageShell from "@/components/layout/page-shell";
 import { ConnectBsky } from "@/components/modals/connect-bsky";
 
 export default async function Page() {
-  console.log("we are connecting an account");
-  const connected = await connectBlueSkyAccount({
-    identifier: "abdellah.example.com",
-    password: "123mahdi",
-  });
-
-  console.log("i think we have finished the connection");
-  console.log(connected);
-  const account = await getConnectedAccount();
-
-  console.log(account.data);
   return (
     <PageShell title="Queue">
       <div className="w-full h-screen flex flex-col justify-center items-center ">
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,11 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+const connectionString = `postgresql://${process.env.DB_USERNAME!}:${process.env
+  .DB_PASSWORD!}@${process.env.DB_HOST!}:${process.env.DB_PORT!}/${process.env
+  .DB_NAME!}?sslmode=${process.env.DB_SSL_MODE}`;
+
+const queryClient = postgres(connectionString);
+
+export const db = drizzle(queryClient, { schema });
