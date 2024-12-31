9c277f1825519be14361f775e7895f75d094b4c7
abdellah <Abdellah.chehri14@gmail.com>
2024-12-31 10:30:10 +0100

    feat: update the codebase

---
+++ src/lib/errors.ts
@@ -0,0 +1,4 @@
+// this will send the error to sentry and will popup an error message here
+// i need to check the type of the error if it is clerk error or a normal error
+
+export const handleError = () => {};
